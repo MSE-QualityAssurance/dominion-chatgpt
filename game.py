d1ff35f0a024d82d22301ef1b3128edafc41bef2
orange04 <512837861@qq.com>
2023-11-29 12:12:51 -0500

    update game

--- game.py
+++ game.py
@@ -5,7 +5,27 @@ class Game:
         self.turn = 0
 
     def setup_supply(self):
-        # Initialize game supply here
+        # Basic setup for 2-4 players
+        num_players = len(self.players)
+
+        # Set up standard Victory and Treasure cards
+        supply = {
+            "Copper": 60 - (7 * num_players),  # Each player starts with 7 Coppers
+            "Silver": 40,
+            "Gold": 30,
+            "Estate": 12 if num_players == 2 else 24,  # 8 per player for 2 players, otherwise 12
+            "Duchy": 12,
+            "Province": 12,
+            "Curse": (num_players - 1) * 10  # 10 per additional player
+        }
+
+        # Add a predefined set of 10 Kingdom cards
+        kingdom_cards = ["Smithy", "Village", "Market", "Militia", "Mine",
+                         "Moat", "Remodel", "Cellar", "Workshop", "Laboratory"]
+        for card in kingdom_cards:
+            supply[card] = 10  # Standard number of each Kingdom card
+
+        return supply
 
     def next_turn(self):
         self.turn = (self.turn + 1) % len(self.players)
